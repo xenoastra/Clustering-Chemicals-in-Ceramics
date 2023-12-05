b2109ffabcbade183a7a1ff27e147ccbb82834e7
Vanessa L <70068643+xenoastra@users.noreply.github.com>
2023-12-04 22:54:10 -0500

    Add files via upload

--- clusteringassignment.py
+++ clusteringassignment.py
@@ -8,33 +8,37 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 data = pd.read_csv(r'D:\desktop 1\Users\Nessie\Documents\coding\Machine Learning\Assignment 4\Chemical Composion of Ceramic.csv')
-
-# Extract features for PCA
-X = data.drop(['Ceramic Name', 'Part', 'P2O5', 'MnO', 'Rb2O', 'SrO', 'ZnO', 'ZrO2', 'PbO2', 'CuO', 'Y2O3', 'SiO2', 'Al2O3', 'CaO'], axis=1)
-
-# Standardize the data
+X = data.drop(['Ceramic Name', 'Part'], axis=1)
 scaler = StandardScaler()
 X_scaled = scaler.fit_transform(X)
-
-# PCA
 pca = PCA()
 X_pca = pca.fit_transform(X_scaled)
 
 # K-Means clustering
-kmeans = KMeans(n_clusters=4, random_state=42, n_init=10)
-labels = kmeans.fit_predict(X_scaled)
+kval = range(1, 10)
+inertias = []
 
-# Visualize in PCA space
-colors = plt.cm.viridis(np.linspace(0, 0.8, len(X.columns)))
+for k in kval:
+    kmeans = KMeans(n_clusters=k, random_state=42)
+    kmeans.fit(X_scaled)
+    inertias.append(kmeans.inertia_)
+
+labels = kmeans.fit_predict(X_scaled)
+colors = plt.cm.viridis(np.linspace(0, 1, len(X.columns)))
 plt.figure(figsize=(10, 8))
 for i, color in zip(range(len(X.columns)), colors):
     plt.scatter(X_pca[labels == i, 0], X_pca[labels == i, 1], c=color, alpha=0.5, label=f'Chemical {i + 1}')
 
-plt.xlabel('Principal Component 1')
-plt.ylabel('Principal Component 2')
+plt.xlabel('chemical')
+plt.ylabel('chemical')
 plt.title('K-Means Clustering for Chemicals in Ceramics (PCA Space)')
+
+plt.xlim(X_pca[:, 0].min(), X_pca[:, 0].max())
+plt.ylim(X_pca[:, 1].min(), X_pca[:, 1].max())
+
 plt.legend()
 plt.show()
+
 # #Hierarcial clustering
 
 # Y = hierarchy.linkage(X.T, 'ward')  # Transpose X to have chemicals as rows and features as columns
